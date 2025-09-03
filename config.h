769de8e3c04e55af011b03c8c3add0b5ef3d595c
Timothy Pidashev <mail@timmypidashev.dev>
2025-09-03 10:34:07 -0700

    update config

--- config.h
+++ config.h
@@ -22,7 +22,6 @@ static const char *colors[][4]      = {
 static const char *const autostart[] = {
   "dwmbar", NULL,
 	"refbar", NULL,
-  "alacritty", NULL,
 	NULL /* terminate */
 };
 
@@ -72,7 +71,7 @@ static const char *lockcmd[]  = { "", NULL };
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY|ShiftMask,		        XK_w,	   spawn,	       {.v = webcmd } },
 	{ MODKEY|ShiftMask,		        XK_l,	   spawn, 	       {.v = lockcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
@@ -84,7 +83,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY,                       XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
